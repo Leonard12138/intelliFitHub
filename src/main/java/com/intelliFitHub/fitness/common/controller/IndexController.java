92a4ebcfe3faa8e2ba88beaea0943107fa06be3d
TonySiu1110 <112227595+TonySiu1110@users.noreply.github.com>
2023-07-18 22:41:37 +0800

    Add Validation on calculator

--- src/main/java/com/intelliFitHub/fitness/common/controller/IndexController.java
+++ src/main/java/com/intelliFitHub/fitness/common/controller/IndexController.java
@@ -10,9 +10,28 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class IndexController {
 
-    @GetMapping({ "/", "/index" })
+	@GetMapping({ "/", "/index" })
 	public String index(Model model) {
 		log.info("*** index");
-    	return "index";
+		return "index";
+	}
+
+	// direct to about page, so on so forth
+	@GetMapping({ "/about" })
+	public String about(Model model) {
+		log.info("*** about");
+		return "about";
+	}
+
+	@GetMapping({ "/blog" })
+	public String blog(Model model) {
+		log.info("*** blog");
+		return "blog";
+	}
+
+	@GetMapping({ "/class" })
+	public String goclass(Model model) {
+		log.info("*** class");
+		return "class";
 	}
 }
--- src/main/java/com/intelliFitHub/fitness/common/dao/domain/TdeeCalculator.java
+++ src/main/java/com/intelliFitHub/fitness/common/dao/domain/TdeeCalculator.java
@@ -2,6 +2,7 @@ package com.intelliFitHub.fitness.common.dao.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
